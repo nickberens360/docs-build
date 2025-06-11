fabc8ea6f3f29b4387ebd4718b3ad956053abb34
Nick Berens <nickberens360@gmail.com>
2025-06-11 14:46:08 -0500

    initial commit

---
+++ assets/BoxDemo-bpkhNbRi.js
@@ -0,0 +1,65 @@
+const n=`<template>
+  <div
+    :style="boxStyle"
+    class="custom-box"
+    @click="handleClick"
+  >
+    {{ title }}
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'CustomBox',
+
+  props: {
+    title: {
+      type: String,
+      default: 'Box'
+    },
+    height: {
+      type: String,
+      default: '150px'
+    },
+    width: {
+      type: String,
+      default: '150px'
+    },
+    bgColor: {
+      type: String,
+      default: '#ff0000'
+    },
+    borderRadius: {
+      type: String,
+      default: '8px'
+    },
+  },
+
+  computed: {
+    boxStyle() {
+      return {
+        width: this.width,
+        height: this.height,
+        backgroundColor: this.bgColor,
+        borderRadius: this.borderRadius,
+      };
+    }
+  },
+
+  methods: {
+    handleClick() {
+      this.$emit('box-clicked');
+    }
+  },
+};
+<\/script>
+
+<style scoped>
+.custom-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+}
+</style>`;export{n as default};
---
+++ assets/SimpleTest-Di0fvKEB.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./SimpleTest.vue_vue_type_script_setup_true_lang-EtLMr3Vx.js";import"./index-DcLyRgA3.js";export{m as default};
